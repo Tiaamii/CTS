00e750969c43e30e9ea3f7182d568afdc794078c
Florentina Necula <florentina.nec@gmail.com>
2015-05-28 21:47:17 +0300

    Update angajatTest.java

--- angajatTest.java
+++ angajatTest.java
@@ -19,8 +19,15 @@ public class angajatTest  extends TestCase{
 		ang = new AngajatBuilder(01,"Ion","Ionescu")
         .setManager(02).setSefDep(03).setNrAct(0).ACTIVARE();
 	}
+	
 
+	//unit test pentru verificat metoda ponteazaActivitate(int nrAct)
+	//testare valori normale (naturale > 0)
+	public void testValoriNormalePontaj(){
+		
+	    //valori de testat
+		int expected = 1;
+		ang.ponteazaActivitate(0);
+		assertEquals("Test ponteazaActivitate()", 1, ang.ponteazaActivitate(1));
+		}
 	}
-	
-	
-}
